851359f65bc5df65f641ce1f12c74c79dc818c5f
Panagiotis Dorgiakis <p.dorgiak@gmail.com>
2017-09-25 12:22:48 +0300

    Added the new routing url for the user to be directed to the profile page

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,13 @@ import {RouterModule, Routes} from "@angular/router";
 
 import { SignUpComponent } from "./authentication/sign-up/sign-up.component"
 import { SignInComponent } from "./authentication/sign-in/sign-in.component"
+import {ProfileComponent} from "./authentication/profile/profile.component";
 
 const APP_ROUTES: Routes = [
     {path: '', redirectTo: '/sign-in', pathMatch: 'full' },
     {path: 'sign-in', component: SignInComponent },
-    {path: 'sign-up', component: SignUpComponent }
+    {path: 'sign-up', component: SignUpComponent },
+    {path: 'user-profile', component: ProfileComponent}
 
 ];
 
